b2421b2607aa2bb93c59227724d581c0dd438adb
Zuhaib Amjad <umar0423@gmail.com>
2023-02-14 14:07:41 +0500

    Update schema and routes for devise

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@1.6.3/lib/esm/index.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
