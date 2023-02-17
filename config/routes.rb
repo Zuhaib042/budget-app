6359d95bd3de50ec128d143ac1df97d822ffdda1
Zuhaib Amjad <umar0423@gmail.com>
2023-02-17 15:45:37 +0500

    Delete commented code from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "splash_screen#index"
   resources :splash_screen , only: [:index]
   devise_scope :user do
     authenticated :user do
